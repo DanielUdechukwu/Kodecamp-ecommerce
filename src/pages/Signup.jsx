8f7370433e34e64c73cc4b9c244455fed6fb397f
Daniel Udechukwu <danieludechukwu117@gmail.com>
2023-11-09 09:34:57 +0100

    :construction: Added full name textfield

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -29,6 +29,22 @@ const Signup = () => {
                 />
               </div>
             </div>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Full Name
+              </label>
+              <div className="mt-1">
+                <input
+                  id="text"
+                  name="text"
+                  type="text"
+                  placeholder="John Doe"
+                  autoComplete="Full Name"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:font-semibold focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2 outline-none"
+                />
+              </div>
+            </div>
 
             <div>
               <div className="flex items-center justify-between">
