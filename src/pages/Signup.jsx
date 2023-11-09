981ca9e75c59fe82cf56abc7f51324cf6566c2e0
Daniel Udechukwu <danieludechukwu117@gmail.com>
2023-11-09 09:42:06 +0100

    :construction: Added full name textfield

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -13,6 +13,23 @@ const Signup = () => {
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-3" action="#" method="POST">
+          <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Full Name
+              </label>
+              <div className="mt-1">
+                <input
+                  id="text"
+                  name="text"
+                  type="text"
+                  placeholder="John Doe"
+                  autoComplete="Full Name"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:font-semibold focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2 outline-none"
+                />
+              </div>
+            </div>
+            
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
@@ -24,22 +41,6 @@ const Signup = () => {
                   type="email"
                   placeholder="JohnDoe@gmail.com"
                   autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:font-semibold focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2 outline-none"
-                />
-              </div>
-            </div>
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Full Name
-              </label>
-              <div className="mt-1">
-                <input
-                  id="text"
-                  name="text"
-                  type="text"
-                  placeholder="John Doe"
-                  autoComplete="Full Name"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:font-semibold focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2 outline-none"
                 />
